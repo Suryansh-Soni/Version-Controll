3d60f2dd6aeeef69c0a302605897d564f423674a
Suryansh Soni <suryanshsoni92@gmail.com>
2026-09-03 05:22:00 +0530

    added commands for add , commit etc.

---
+++ Backend/index.js
@@ -0,0 +1,23 @@
+const yargs=require('yargs');
+const { hideBin } = require('yargs/helpers');
+const { initRepo } = require('./controllers/init');
+const { addRepo } = require('./controllers/add');
+yargs(hideBin(process.argv))
+  .command('init', 'Initialize the repository',  {}, initRepo)
+  .command('add <file>', 'Add files to the index',(yargs)=>{yargs.positional("file",{describe: "The file to add to stagging area.",type:"string",})}, addRepo)
+  .command('commit <message>', 'Commit changes to the repository', (yargs) => {
+    yargs.positional('message', {
+      describe: 'The commit message',
+      type: 'string',
+    })})
+    .command('push', 'Push changes to the remote repository', {}, pushRepo)
+    .command('pull', 'Pull changes from the remote repository', {}, pullRepo)
+    .command('revert <commitID>', 'Revert to a specific commit', (yargs) => {
+      yargs.positional('commitID', {
+        describe: 'The commit hash to revert to',
+        type: 'string',
+      });
+    })
+  
+  .demandCommand(1, 'You need at least one command before moving on')
+  .help().argv;
