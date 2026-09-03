57d1986bd9e902771719a3e28566f57030c732a5
Suryansh Soni <suryanshsoni92@gmail.com>
2026-09-03 05:41:12 +0530

    made functional add fn

--- Backend/controllers/add.js
+++ Backend/controllers/add.js
@@ -1,6 +1,18 @@
-async function add(){
-    cconsole.log("Add called with file: ")}
+const fs = require('fs');
+const path = require('path');
 
+async function add(filePath){
+    const repoPath = path.resolve(process.cwd(), '.myvcs');
+    const stagingPath = path.join(repoPath, 'staging');
+    try{
+        await fs.promises.mkdir(stagingPath, { recursive: true });
+        const fileName=path.basename(filePath);
+        await fs.promises.copyFile(filePath, path.join(stagingPath, fileName));
+        console.log("File added to staging area: " + fileName);
+    }catch (err) {
+        console.error("Error adding file to staging area: ", err);
+    }
+}
 module.exports = {
     addRepo: add
 }   
