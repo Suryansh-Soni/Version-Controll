c5b714e70457ce24e6f86d2e9c5cb3d1c1f4873d
Suryansh Soni <suryanshsoni92@gmail.com>
2026-09-03 06:24:37 +0530

    updated index

--- Backend/index.js
+++ Backend/index.js
@@ -1,29 +1,78 @@
-const yargs=require('yargs');
+const yargs = require('yargs');
 const { hideBin } = require('yargs/helpers');
+
 const { initRepo } = require('./controllers/init');
 const { addRepo } = require('./controllers/add');
 const { commitRepo } = require('./controllers/commit');
 const { pushRepo } = require('./controllers/push');
 const { pullRepo } = require('./controllers/pull');
 const { revertRepo } = require('./controllers/revert');
+
 yargs(hideBin(process.argv))
-  .command('init', 'Initialize the repository',  {}, initRepo)
-  .command('add <file>', 'Add files to the index',(yargs)=>{yargs.positional("file",{describe: "The file to add to stagging area.",type:"string",})}, (argv)=>{
-    addRepo(argv.file);
-  })
-  .command('commit <message>', 'Commit changes to the repository', (yargs) => {
-    yargs.positional('message', {
-      describe: 'The commit message',
-      type: 'string',
-    })})
-    .command('push', 'Push changes to the remote repository', {}, pushRepo)
-    .command('pull', 'Pull changes from the remote repository', {}, pullRepo)
-    .command('revert <commitID>', 'Revert to a specific commit', (yargs) => {
+
+  .command(
+    'init',
+    'Initialize the repository',
+    {},
+    initRepo
+  )
+
+  .command(
+    'add <file>',
+    'Add files to the index',
+    (yargs) => {
+      yargs.positional('file', {
+        describe: 'The file to add to staging area.',
+        type: 'string',
+      });
+    },
+    (argv) => {
+      addRepo(argv.file);
+    }
+  )
+
+  .command(
+    'commit <message>',
+    'Commit changes to the repository',
+    (yargs) => {
+      yargs.positional('message', {
+        describe: 'The commit message',
+        type: 'string',
+      });
+    },
+    (argv) => {
+      commitRepo(argv.message);
+    }
+  )
+
+  .command(
+    'push',
+    'Push changes to the remote repository',
+    {},
+    pushRepo
+  )
+
+  .command(
+    'pull',
+    'Pull changes from the remote repository',
+    {},
+    pullRepo
+  )
+
+  .command(
+    'revert <commitID>',
+    'Revert to a specific commit',
+    (yargs) => {
       yargs.positional('commitID', {
         describe: 'The commit hash to revert to',
         type: 'string',
       });
-    })
-  
+    },
+    (argv) => {
+      revertRepo(argv.commitID);
+    }
+  )
+
   .demandCommand(1, 'You need at least one command before moving on')
-  .help().argv;
+  .help()
+  .argv;
