723f1d34eb2192bab3968f27ca02e3008916b48f
Suryansh Soni <suryanshsoni92@gmail.com>
2026-09-03 21:10:47 +0530

    new aws config

--- Backend/config/aws-config.js
+++ Backend/config/aws-config.js
@@ -1,8 +1,17 @@
-const AWS=require('aws-sdk');
-AWS.config.update({"region":"ap-south-1"});
-const s3=new AWS.S3();
-const s3_BUCKET="samplebucket050205";
-module.exports={
+
+const AWS = require("aws-sdk");
+
+AWS.config.update({
+    region: process.env.AWS_REGION,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const s3 = new AWS.S3();
+
+const s3_BUCKET = process.env.AWS_BUCKET_NAME;
+
+module.exports = {
     s3,
     s3_BUCKET
-}
+};
