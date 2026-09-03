d66f08a58a36d684cd8cb81f59c5904638c1471d
Suryansh Soni <suryanshsoni92@gmail.com>
2026-09-03 17:18:37 +0530

    functional push fn

--- Backend/controllers/push.js
+++ Backend/controllers/push.js
@@ -1,5 +1,31 @@
-function pushRepo(){
-    console.log("Push called.");
+const fs=require('fs');
+const path=require('path');
+const {s3,s3_BUCKET}=require('../config/aws-config');   
+
+async function pushRepo(){
+    const repoPath=path.resolve(process.cwd(),'.myvcs');
+    const commitsPath=path.join(repoPath,'commits');
+    try{
+        const commitDirs=await fs.promises.readdir(commitsPath);
+        for(const commitDir of commitDirs){
+            const commitDirPath=path.join(commitsPath,commitDir);  
+            const files=await fs.promises.readdir(commitDirPath);
+            for(const file of files){
+                const filePath=path.join(commitDirPath,file);
+                const fileContent=await fs.promises.readFile(filePath);
+                const params={
+                    Bucket:s3_BUCKET,
+                    Key:`${commitDir}/${file}`,
+                    Body:fileContent
+                };
+                await s3.upload(params).promise();
+                
+                }
+        }
+        console.log(`File ${file} from commit ${commitDir} pushed to S3 successfully.`);
+    }catch(err){
+        console.error("Error pushing changes to S3: ",err);
+    }   
 }
 module.exports = {
     pushRepo
