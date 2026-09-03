032b0a3da8d77acf46d5ba1d0b4a942798968d11
Suryansh Soni <suryanshsoni92@gmail.com>
2026-09-03 05:33:34 +0530

    functional init function

--- Backend/controllers/init.js
+++ Backend/controllers/init.js
@@ -1,5 +1,19 @@
-function initRepo(){
-    console.log("Init called.");
+const fs = require('fs').promises;
+const path = require('path');
+
+
+
+async function initRepo(){
+    const repoPath = path.resolve(process.cwd(), '.myvcs'); 
+    const commitPath = path.join(repoPath, 'commits');
+    try{
+        await fs.mkdir(repoPath, { recursive: true }); //recursive: true allows creating nested directories
+        await fs.mkdir(commitPath, { recursive: true });
+        await fs.writeFile(path.join(repoPath, 'config.json'), JSON.stringify({bucket:process.env.S3_BUCKET})); // Initialize an empty index file
+        console.log("Repository initialized successfully at: " + repoPath);
+    }catch (err) {
+        console.error("Error initializing repository: ", err);
+    }  
 }
 module.exports = {
     initRepo
